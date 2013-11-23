c9b82f76ae86f939a22cee0c808931ff1117dd31
Bartek <czaber@gmail.com>
2013-11-23 04:45:05 +0100

    Zakomentuj opcjonalne linie

--- backup.sh
+++ backup.sh
@@ -25,12 +25,11 @@ BASEDIR='/backup'
 FULLDIR="${BASEDIR}/${PERIOD}"
 DATE=`date +%Y%m%d%H%M%S`
 
-R="--exclude home/redmine/redmine/tmp"
-G="--exclude home/git/gitlab/tmp"
+#R="--exclude home/redmine/redmine/tmp"
 TAROPT="-pczf"
 RSYNCOPT="-az -H --delete"
-DUMPOPT="--defaults-extra-file=/root/etc/backup.cnf"
-ROOTDUMPOPT="--defaults-extra-file=/root/etc/root.cnf --ignore-table=mysql.event"
+#DUMPOPT="--defaults-extra-file=/root/etc/backup.cnf"
+#ROOTDUMPOPT="--defaults-extra-file=/root/etc/root.cnf --ignore-table=mysql.event"
 
 # 2. katalogi robocze
 mkdir -p "$FULLDIR"
@@ -38,16 +37,12 @@ mkdir -p "$FULLDIR"
 # 3. backup
 cd $FULLDIR
 
-tar    $TAROPT ${DATE}_owncloud.tgz -C / home/owncloud
-tar $R $TAROPT ${DATE}_redmine.tgz -C / home/redmine
-tar $G $TAROPT ${DATE}_gitlab.tgz -C / home/git
+#tar $R $TAROPT ${DATE}_redmine.tgz -C / home/redmine
 tar    $TAROPT ${DATE}_root.tgz  -C / root
 tar    $TAROPT ${DATE}_etc.tgz  -C / etc
 
-mysqldump $DUMPOPT -u owncloud owncloud_production | gzip > ${DATE}_owncloud.sql.gz
-mysqldump $DUMPOPT -u gitlab   gitlabhq_production | gzip > ${DATE}_gitlab.sql.gz
-mysqldump $DUMPOPT -u redmine  redmine_production  | gzip > ${DATE}_redmine.sql.gz
-mysqldump $ROOTDUMPOPT -u root mysql | gzip > ${DATE}_backup.sql.gz
+#mysqldump $DUMPOPT -u redmine  redmine_production  | gzip > ${DATE}_redmine.sql.gz
+#mysqldump $ROOTDUMPOPT -u root mysql | gzip > ${DATE}_backup.sql.gz
 
 dpkg --get-selections > ./${DATE}_installed-packages
 
