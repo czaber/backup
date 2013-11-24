ee68d646d326214390b7c1bdc5217bc252abfef2
Bartek <czaber@gmail.com>
2013-11-24 20:00:11 +0100

    Popraw błąd z nieinterpretowanym \n przez echo

--- report.sh
+++ report.sh
@@ -4,30 +4,31 @@
 # 0 6    * * 7 /root/bin/report.sh
 
 LSOPT='-1st --si'
+SMARTCTL=/usr/sbin/smartctl
 
 echo "Stan dysku"
 echo "=========="
 df -H -x tmpfs -x devtmpfs
 echo "..."
 echo "S.M.A.R.T /dev/sda"
-smartctl -l selftest /dev/sda | grep '#'
+$SMARTCTL -l selftest /dev/sda | grep '#'
 echo "S.M.A.R.T /dev/sdb"
-smartctl -l selftest /dev/sdb | grep '#'
+$SMARTCTL -l selftest /dev/sdb | grep '#'
 echo "S.M.A.R.T /dev/sdc"
-smartctl -l selftest /dev/sdc | grep '#'
-echo "...\n"
+$SMARTCTL -l selftest /dev/sdc | grep '#'
+echo -e "...\n"
 
 echo "Dzienne kopie"
 echo "============="
 ls $LSOPT /backup/daily/ | head -n 72 # 7 x 10 files per day + 1 header line + 1 extra file
-echo "...\n"
+echo -e "...\n"
 
 echo "Tygodniowe kopie"
 echo "================"
 ls $LSOPT /backup/weekly/ | head -n 42 # 4 x 10 files per day + 1 header line + 1 extra file
-echo "...\n"
+echo -e "...\n"
 
 echo "Miesięczne kopie"
 echo "================"
 ls $LSOPT /backup/monthly/ | head -n 22 # 2 x 10 files per day + 1 header line + 1 extra file
-echo "...\n"
+echo -e "...\n"
